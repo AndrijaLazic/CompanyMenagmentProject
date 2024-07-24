30deb4fabc3ce1f04bed81e37dea12a811b59355
AndrijaLazic <lazic.andrija223@gmail.com>
2024-07-24 21:10:55 +0200

    Implemented footer

--- FrontEnd/frontend-side-project/src/app/app.component.ts
+++ FrontEnd/frontend-side-project/src/app/app.component.ts
@@ -1,14 +1,14 @@
-import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavBarComponent } from './core/components/nav-bar/nav-bar.component';
+import { FootBarComponent } from './core/components/foot-bar/foot-bar.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
   standalone: true,
-  imports: [RouterOutlet, NavBarComponent],
+  imports: [RouterOutlet, NavBarComponent, FootBarComponent],
 })
 export class AppComponent {
   title = 'frontend-side-project';
